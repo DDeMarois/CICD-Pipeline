d151c3003d304ecc9891888948c68f0cfe89e268
DDeMarois <Drew.DeMarois@du.edu>
2025-02-01 21:03:35 -0700

    Refactor seed script to read Python questions from JSON file

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,14 +1,16 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
+import { readFileSync } from "fs";
 
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+const pythonQuestions = JSON.parse(
+  readFileSync(new URL("./pythonQuestions.json", import.meta.url), "utf8")
+);
 
-db.once('open', async () => {
-  await cleanDB('Question', 'questions');
-
+db.once("open", async () => {
+  await cleanDB("Question", "questions");
   await Question.insertMany(pythonQuestions);
 
-  console.log('Questions seeded!');
+  console.log("Questions seeded!");
   process.exit(0);
 });
